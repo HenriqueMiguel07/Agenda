c87dd25e1fb486a551013dc416d6810d53dd20e7
HenriqueMiguel07 <miguelhenriquecardosoj2@gmail.com>
2026-03-29 18:11:42 -0300

    Rename localStorage keys for data persistence

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // script.js
 
+// Salva os dados no "HD" do navegador
 function salvarDados(dados) {
-    localStorage.setItem('minha_agenda_data', JSON.stringify(dados));
+    localStorage.setItem('minha_agenda_persistencia', JSON.stringify(dados));
 }
 
+// Busca os dados salvos. Se não houver nada, retorna null.
 function carregarDados() {
-    const dados = localStorage.getItem('minha_agenda_data');
+    const dados = localStorage.getItem('minha_agenda_persistencia');
     return dados ? JSON.parse(dados) : null;
 }
