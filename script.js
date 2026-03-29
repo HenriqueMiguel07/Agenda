f9f6eb08e00fe096bea46149db748a27cc86931c
HenriqueMiguel07 <miguelhenriquecardosoj2@gmail.com>
2026-03-29 14:46:06 -0300

    Refactor salvarDados and carregarDados functions

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 // script.js
 
-// Função para salvar (chame sempre que o valor mudar)
-export const salvarDados = (valor) => {
-    localStorage.setItem('dados_zenblocks', JSON.stringify(valor));
-    console.log("Dados salvos no localstorage!");
-};
+function salvarDados(dados) {
+    localStorage.setItem('minha_agenda_data', JSON.stringify(dados));
+}
 
-// Função para carregar (chame ao iniciar o app)
-export const carregarDados = () => {
-    const dados = localStorage.getItem('dados_zenblocks');
+function carregarDados() {
+    const dados = localStorage.getItem('minha_agenda_data');
     return dados ? JSON.parse(dados) : null;
-};
+}
